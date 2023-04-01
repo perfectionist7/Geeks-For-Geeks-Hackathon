42f3d874adde4d8c3e0df8e78d56ac62a9a5998a
Ayush Khandelwal <akhandelwal2003@gmail.com>
2023-04-02 00:32:50 +0530

    registration page inital

---
+++ chatbot_app/lib/registration.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/backgroundimage.png"),
+                fit: BoxFit.cover),
+          ),
+          child: ListView(
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              children: [
+                Container(),
+              ]),
+        ),
+      ),
+    );
+  }
+}
