72f26fc8e3256222c84fab316c4f0383e9fb7d7a
perfectionist7 <96302137+perfectionist7@users.noreply.github.com>
2023-04-08 09:18:44 +0530

    Update signup_doctor.dart

--- chatbot_app/lib/signup_doctor.dart
+++ chatbot_app/lib/signup_doctor.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'registration.dart';
+import 'login.dart';
 
+class DoctorUserPage extends StatefulWidget {
+  const DoctorUserPage({Key? key}) : super(key: key);
+
+  @override
+  State<DoctorUserPage> createState() => _DoctorUserState();
+}
+
+class _DoctorUserState extends State<DoctorUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/backgroundimage.png"),
+                      fit: BoxFit.cover),
+                ),
+                child: ListView(
+                    physics: const BouncingScrollPhysics(
+                        parent: AlwaysScrollableScrollPhysics()),
+                    children: [
+                      Container(),
+                    ]))));
+  }
+}
