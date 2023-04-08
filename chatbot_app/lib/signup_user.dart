022ef1dfb079bcb1c264d4d4e28b8ad0cc0d80a8
kushm121 <makkarkush@gmail.com>
2023-04-08 23:11:04 +0530

    sign up user changes

--- chatbot_app/lib/signup_user.dart
+++ chatbot_app/lib/signup_user.dart
@@ -25,7 +25,109 @@ class _SignUserState extends State<SignUserPage> {
                     physics: const BouncingScrollPhysics(
                         parent: AlwaysScrollableScrollPhysics()),
                     children: [
-                      Container(),
+                      Row(children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(27, 84, 0, 0),
+                  height: 28,
+                  width: 80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'User',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Inter',
+                        fontSize: 15,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32)),
+                      ), 
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(19, 84, 200, 0),
+                  height: 28,
+                  width: 85,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32)),
+                      ),
+                      onPressed: (){},
+                    child: Text(
+                      'Doctor',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Inter',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(42, 40, 180, 0),
+                    child: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Container(
+                    height: 33,
+                    width: 277,
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    margin: EdgeInsets.fromLTRB(38, 8, 96, 615),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Phone Number',
+                        contentPadding: EdgeInsets.all(10),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(78, 39, 198, 0),
+                    child: Text(
+                      'Full Name',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Container(
+                    height: 33,
+                    width: 277,
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    margin: EdgeInsets.fromLTRB(70, 5, 98, 0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Full Name',
+                        contentPadding: EdgeInsets.all(10),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
                     ]))));
   }
 }
