0a11a49a530fe7b469709f523c3db60102576d73
perfectionist7 <96302137+perfectionist7@users.noreply.github.com>
2023-04-08 09:34:48 +0530

    Update signup_doctor.dart

--- chatbot_app/lib/signup_doctor.dart
+++ chatbot_app/lib/signup_doctor.dart
@@ -25,7 +25,84 @@ class _DoctorUserState extends State<DoctorUserPage> {
                     physics: const BouncingScrollPhysics(
                         parent: AlwaysScrollableScrollPhysics()),
                     children: [
-                      Container(),
-                    ]))));
+              Row(children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(27, 84, 0, 0),
+                  height: 28,
+                  width: 80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'User',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Inter',
+                        fontSize: 15,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                  
+                      ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(19, 84, 200, 0),
+                  height: 28,
+                  width: 85,
+                  child: ElevatedButton(
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LandingPage()),
+                      );
+                    },
+                    child: Text(
+                      'Doctor',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Inter',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(42, 40, 180, 0),
+                    child: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Container(
+                    height: 33,
+                    width: 277,
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    margin: EdgeInsets.fromLTRB(38, 8, 96, 615),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Phone Number',
+                        contentPadding: EdgeInsets.all(10),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ),
+         ]))));
   }
 }
